06237391867d1f073f87a5662158155f9cde0693
Renan Andrade <eu@renanandrade.com.br>
2017-12-31 18:51:20 -0200

    add botstrapvue and navbar component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueFire from 'vuefire'
+import BootstrapVue from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(VueFire)
+Vue.use(BootstrapVue)
 
 new Vue({
   el: '#app',
