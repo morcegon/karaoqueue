b0bb8d30eee74ce94f7c3ec8bd441c9a8d0b1535
Renan Andrade <eu@renanandrade.com.br>
2017-12-31 20:01:33 -0200

    add result component

---
+++ src/components/Results.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="row">
+    <div
+      :key="index"
+      v-for="(video, index) in videos"
+      class="card">
+        <div class="img-container">
+          <img :src="video.snippet.thumbnails.medium.url">
+        </div>
+
+        <div class="card-body">
+          <h5>{{ video.snippet.title }}</h5>
+          <a
+            class="btn btn-sm btn-info">Adicionar à Fila</a>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['videos']
+}
+</script>
+
+<style lang="sass" scoped>
+  .row
+    display: flex
+    justify-content: space-around
+
+  .card
+    padding: 0
+    margin-bottom: 15px
+    width: 45%
+
+  .card-body
+    padding: 5px 10px
+
+  .img-container
+    align-content: center
+    background-color: black
+    display: flex
+    height: 90px
+    justify-content: center
+
+    img
+      max-height: 100%
+      max-width: 100%
+
+  h5
+    font-size: 1rem
+
+  a
+    color: white !important
+    width: 100%
+</style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,3 +1,67 @@
 <template>
-  <div>Busca</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="form-group">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Busca"
+            @input="searchMusic"
+            v-model="keyword">
+        </div>
+      </div>
+    </div>
+
+    <app-results
+      v-if="results"
+      :videos="results"></app-results>
+  </div>
 </template>
+
+<script>
+import axios from 'axios'
+import results from './Results'
+
+export default {
+  data() {
+    return {
+      keyword: null,
+      results: null,
+      karaokeOnly: true
+    }
+  },
+  methods: {
+    searchMusic () {
+      const baseUrs = 'https://www.googleapis.com/youtube/v3/search'
+      const apiKey = 'AIzaSyD5lcXLJGhyiRdRNPcfT8ZJyEkSUadEi6E'
+
+      let keyword = (this.karaokeOnly ? this.keyword + ' + karaoke' : this.keyword)
+
+      if (this.keyword !== null && this.keyword.length > 2) {
+        axios
+          .get(baseUrs, {
+            params: {
+              part: 'snippet',
+              q: keyword,
+              type: 'video',
+              videoEmbeddable: 'true',
+              maxResults: 20,
+              key: apiKey
+            }
+          })
+          .then(result => {
+            this.results = result.data.items
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
+    }
+  },
+  components: {
+    appResults: results
+  }
+}
+</script>
+
